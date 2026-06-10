5e5e5fa3a26152f52f9b5d48c03fecbcc82c48f5
achanyou2 <ariane.chanyou@gmail.com>
2026-06-10 11:55:23 -0400

    Add practice task

---
+++ HealthArticle/news_article.js
@@ -0,0 +1,29 @@
+var xhr = new XMLHttpRequest();
+var url = './news_article.json';
+xhr.open('GET', url, true);
+xhr.responseType = 'json';
+
+xhr.onload = function() {
+var articles = xhr.response.articles;
+var articlesDiv = document.getElementById('news_articles');
+
+  articles.forEach(function(article) {
+      var articleDiv = document.createElement('div');
+      articleDiv.classList.add('article');
+
+      var title = document.createElement('h2');
+      title.textContent = article.title;
+
+      var description = document.createElement('p');
+      description.textContent = article.description;
+
+      articleDiv.appendChild(title);
+      articleDiv.appendChild(description);
+
+      articlesDiv.appendChild(articleDiv);
+    });
+}
+
+xhr.send();
+
+
